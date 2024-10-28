6a1f81d8465399819588fda5d5ce2e2377669443
moznion <moznion@mail.moznion.net>
2024-10-28 17:49:25 +0900

    Initial commit
    Signed-off-by: moznion <moznion@mail.moznion.net>

---
+++ lib/neologd/normalizer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "normalizer/version"
+
+module Neologd
+  module Normalizer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/neologd/normalizer/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Neologd
+  module Normalizer
+    VERSION = "0.1.0"
+  end
+end
