2c2f003cd648a4e5e38fd35c5734c23d76a67033
moznion <moznion@mail.moznion.net>
2024-10-29 10:28:31 +0900

    Fix some corner cases on normalization
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- test/neologdish/test_normalizer.rb
+++ test/neologdish/test_normalizer.rb
@@ -18,6 +18,7 @@ module Neologdish
         assert_equal 'majikaー', Neologdish::Normalizer.normalize('majika━')
         assert_equal 'わい', Neologdish::Normalizer.normalize('わ〰い')
         assert_equal 'スーパー', Neologdish::Normalizer.normalize('スーパーーーー')
+        assert_equal 'スーパーマーケット', Neologdish::Normalizer.normalize('スーパーーーーマーーーーケット')
         assert_equal '!#', Neologdish::Normalizer.normalize('!#')
         assert_equal 'ゼンカクスペース', Neologdish::Normalizer.normalize('ゼンカク　スペース')
         assert_equal 'おお', Neologdish::Normalizer.normalize('お             お')
@@ -27,10 +28,15 @@ module Neologdish
         assert_equal 'アルゴリズムC', Neologdish::Normalizer.normalize('アルゴリズム C')
         assert_equal 'PRML副読本', Neologdish::Normalizer.normalize('　　　ＰＲＭＬ　　副　読　本　　　')
         assert_equal 'Coding the Matrix', Neologdish::Normalizer.normalize('Coding the Matrix')
+        assert_equal 'consecutive spaces are in latin words',
+                     Neologdish::Normalizer.normalize('consecutive  spaces   are    in     latin words')
+        assert_equal 'full width spaces are in latin words',
+                     Neologdish::Normalizer.normalize('full　width　　spaces　　　are　　　　in　latin　words')
         assert_equal '南アルプスの天然水Sparking Lemonレモン一絞り',
                      Neologdish::Normalizer.normalize('南アルプスの　天然水　Ｓｐａｒｋｉｎｇ　Ｌｅｍｏｎ　レモン一絞り')
         assert_equal '南アルプスの天然水-Sparking*Lemon+レモン一絞り',
                      Neologdish::Normalizer.normalize('南アルプスの　天然水-　Ｓｐａｒｋｉｎｇ*　Ｌｅｍｏｎ+　レモン一絞り')
+        assert_equal 'ツギノ「ヌﾟ」ハオカシナモジデス', Neologdish::Normalizer.normalize('ﾂｷﾞﾉ「ﾇﾟ」ﾊｵｶｼﾅﾓｼﾞﾃﾞｽ')
       end
     end
   end
